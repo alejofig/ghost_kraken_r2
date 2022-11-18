165de76bc864ec9d58cad25c07c84fdad07b5c62
Johnattan Devia <jd@ingenio.com.co>
2022-11-18 15:51:23 -0500

    Adapted versions with creation and deletes test admin

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,11 +27,24 @@
 import {faker} from "@faker-js/faker";
 
 Cypress.Commands.add('goIntoSettings', (settings,feature = "general",ghost_version = "new") => {
-    cy.get('.gh-nav-bottom a[href="#/settings/"]').click()
-    cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_ghost_settings`)
-    cy.get('.gh-main a[href="#/settings/'+settings+'/"]').click()
+
+    if (ghost_version === 'new'){
+        cy.get('.gh-nav-bottom a[href="#/settings/"]').click()
+        cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_ghost_settings`)
+
+        cy.get('.gh-main a[href="#/settings/'+settings+'/"]').click()
+        cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_ghost_settings_${settings}`)
+        return
+    }
+
+    cy.get('.gh-nav-body a[href="#/settings/'+settings+'/"]').click()
     cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_ghost_settings_${settings}`)
+
+
+
 })
+
+
 
 Cypress.Commands.add('saveSettings', () => {
     cy.get('.view-actions button').click()
