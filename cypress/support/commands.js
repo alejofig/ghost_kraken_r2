cc874084d3cfe9de866edb9483388f79446d249b
jcamilorg <juancamilorg64@gmail.com>
2022-11-18 21:48:06 -0500

    Tags edited to take screenshoot

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,66 +24,67 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import {faker} from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 
-Cypress.Commands.add('goIntoSettings', (settings,feature = "general",ghost_version = "new") => {
-
-    if (ghost_version === 'new'){
-        cy.get('.gh-nav-bottom a[href="#/settings/"]').click()
-        cy.get('.gh-main a[href="#/settings/'+settings+'/"]').click()
-        cy.wait(1000)
-        cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_ghost_settings_${settings}`)
-        return
+Cypress.Commands.add(
+  "goIntoSettings",
+  (settings, feature = "general", ghost_version = "new") => {
+    if (ghost_version === "new") {
+      cy.get('.gh-nav-bottom a[href="#/settings/"]').click();
+      cy.get('.gh-main a[href="#/settings/' + settings + '/"]').click();
+      cy.wait(1000);
+      cy.screenshot(
+        `images/cypress/${feature}_${ghost_version}/navigate_ghost_settings_${settings}`
+      );
+      return;
     }
 
-    cy.get('.gh-nav-body a[href="#/settings/'+settings+'/"]').click()
-    cy.wait(1000)
-    cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_ghost_settings_${settings}`)
+    cy.get('.gh-nav-body a[href="#/settings/' + settings + '/"]').click();
+    cy.wait(1000);
+    cy.screenshot(
+      `images/cypress/${feature}_${ghost_version}/navigate_ghost_settings_${settings}`
+    );
+  }
+);
 
+Cypress.Commands.add("saveSettings", () => {
+  cy.get(".view-actions button").click();
+});
 
+Cypress.Commands.add(
+  "goAdminAndLogin",
+  (feature = "general", ghost_version = "new") => {
+    const username = "jfdeviar@gmail.com";
+    const password = "pt100UNAL--";
 
-})
-
-
-
-Cypress.Commands.add('saveSettings', () => {
-    cy.get('.view-actions button').click()
-})
-
-Cypress.Commands.add('goAdminAndLogin', (feature = "general",ghost_version = "new") => {
-    const username = 'jfdeviar@gmail.com'
-    const password = 'pt100UNAL--'
-
-    let endpoint = 'http://uniandes.ingenio.com.co:2368/ghost'
-    if (ghost_version === 'old') {
-        endpoint = 'http://uniandes.ingenio.com.co:2367/ghost'
+    let endpoint = "http://uniandes.ingenio.com.co:2368/ghost";
+    if (ghost_version === "old") {
+      endpoint = "http://uniandes.ingenio.com.co:2367/ghost";
     }
 
-    cy.visit(endpoint)
-    cy.wait(1000)
-    cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_ghost_admin`)
-    cy.get('input.email').type(username)
-    cy.get('input.password').type(password)
-    cy.get('button.login').click() // Click on button
-    cy.wait(1000)
-    cy.screenshot(`images/cypress/${feature}_${ghost_version}/login_admin`)
-})
+    cy.visit(endpoint);
+    cy.wait(1000);
+    cy.screenshot(
+      `images/cypress/${feature}_${ghost_version}/navigate_ghost_admin`
+    );
+    cy.get("input.email").type(username);
+    cy.get("input.password").type(password);
+    cy.get("button.login").click(); // Click on button
+    cy.wait(1000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/login_admin`);
+  }
+);
 
-Cypress.Commands.add('goWebsite', (feature = "general",ghost_version = 'new') => {
-    
-    let endpoint = 'http://uniandes.ingenio.com.co:2368'
-    if (ghost_version === 'old') {
-        endpoint = 'http://uniandes.ingenio.com.co:2367'
+Cypress.Commands.add(
+  "goWebsite",
+  (feature = "general", ghost_version = "new") => {
+    let endpoint = "http://uniandes.ingenio.com.co:2368";
+    if (ghost_version === "old") {
+      endpoint = "http://uniandes.ingenio.com.co:2367";
     }
 
-    cy.visit(endpoint)
-<<<<<<< HEAD
-    cy.wait(1000)
-    cy.screenshot(`images/cypress/${feature}_${ghost_version}/new_site`)
-
-
-=======
-    cy.screenshot(`images/cypress/${feature}_${ghost_version}/main_website`)
-    cy.wait(1000)
->>>>>>> e49c608cfb87c87e312e5875ae9b47bbd0d2db9c
-})
+    cy.visit(endpoint);
+    cy.wait(1000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/new_site`);
+  }
+);
