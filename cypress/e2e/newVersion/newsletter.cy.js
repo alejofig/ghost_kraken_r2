7adf459564390f94ed5c58dad8b978f4b1402e4b
bemejia <b.mejial@uniandes.edu.co>
2022-11-20 12:45:06 -0500

    cypress screenshots complete

--- cypress/e2e/newVersion/newsletter.cy.js
+++ cypress/e2e/newVersion/newsletter.cy.js
@@ -1,28 +1,37 @@
 import {faker} from "@faker-js/faker";
 
-describe('Admin add newsletter', () => {
+describe('Admin add newsletter', (feature = "page", ghost_version = "new") => {
 
   it('Como usuario administrador me logeo e intento agregar boletin, indicar nombre, decripción y crear', () => {
     const title = faker.name.jobTitle();
     const description = faker.lorem.paragraph();
-    cy.goAdminAndLogin()
+    cy.intercept("/ghost/api/admin/settings").as("saveSettings");
+    cy.goAdminAndLogin(feature)
     cy.get('.gh-nav-bottom a[href="#/settings/"]').click()
     cy.get('.gh-main a[href="#/settings/newsletters/]').click()
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_newsletter`);
     cy.get('#newsletter-title').type(title);
     cy.get("textarea.gh-input").type(description);
     cy.get('button.gh-btn-primary').click()
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/add_newsletter`);
 
   })
 
   it('Como usuario administrador me logeo e intento agregar boletin, indicar nombre, decripción y cancelar', () => {
     const title = faker.name.jobTitle();
     const description = faker.lorem.paragraph();
-    cy.goAdminAndLogin()
-    cy.wait(5000)
-    cy.get('li a[href="#/settings/newsletters/"]').click()
-    cy.get('li a[href="#/settings/newsletters/new"]').click()
+    cy.intercept("/ghost/api/admin/settings").as("saveSettings");
+    cy.goAdminAndLogin(feature)
+    cy.get('.gh-nav-bottom a[href="#/settings/"]').click()
+    cy.get('.gh-main a[href="#/settings/newsletters/]').click()
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_newsletter_cancel`);
     cy.get('#newsletter-title').type(title);
     cy.get("textarea.gh-input").type(description);
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/cancel_newsletter`);
     cy.get('button.gh-btn').click()
 
   })
--- cypress/e2e/newVersion/page.cy.js
+++ cypress/e2e/newVersion/page.cy.js
@@ -1,21 +1,33 @@
 import { faker } from "@faker-js/faker";
 
-describe("Admin create/cancel/edit page", () => {
+describe("Admin create/cancel/edit page", (feature = "page", ghost_version = "new") => {
   it("Como usuario administrador me logeo e intento crear una pagina, insertar titulo, descripcion, publicar ahora mismo", () => {
     const title = faker.name.jobTitle();
     const description = faker.lorem.paragraph();
-    cy.goAdminAndLogin();
+    cy.intercept("/ghost/api/admin/settings").as("saveSettings");
+    cy.goAdminAndLogin(feature);
+    cy.wait(3000);
     cy.visit("http://uniandes.ingenio.com.co:2368/ghost/#/pages/");
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_page`);
     cy.get("a.ember-view.gh-btn.gh-btn-primary.view-actions-top-row")
       .contains("New page")
       .click();
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/create_page`);  
     cy.get("textarea.gh-editor-title").type(title);
     cy.get("article.koenig-editor").type(description);
     cy.get("button.gh-publish-trigger").click();
     cy.get("button.gh-btn.gh-btn-black.gh-btn-large").click();
     cy.get("button.gh-btn.gh-btn-pulse").click();
+    cy.wait(3000);
+    cy.screenshot(
+      `images/cypress/${feature}_${ghost_version}/create_page_success`
+    );    
     let titleUrl = title.replaceAll(" ", "-").toLowerCase();
     cy.visit(`http://uniandes.ingenio.com.co:2368/${titleUrl}/`);
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/published_page`);
     cy.get("h1.single-title").should("have.text", title);
     cy.get("div.single-content p").should("have.text", description);
   });
@@ -23,30 +35,54 @@ describe("Admin create/cancel/edit page", () => {
   it("Como usuario administrador me logeo e intento crear una pagina, insertar titulo, descripcion y cancelar", () => {
     const title = faker.name.jobTitle();
     const description = faker.lorem.paragraph();
-    cy.goAdminAndLogin();
+    cy.intercept("/ghost/api/admin/settings").as("saveSettings");
+    cy.goAdminAndLogin(feature);
+    cy.wait(3000);
     cy.visit("http://uniandes.ingenio.com.co:2368/ghost/#/pages/");
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/navigate_page_cancel`);
     cy.get("a.ember-view.gh-btn.gh-btn-primary.view-actions-top-row")
       .contains("New page")
       .click();
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/create_page_cancel`);
     cy.get("textarea.gh-editor-title").type(title);
     cy.get("article.koenig-editor").type(description);
     cy.wait(5000)
     cy.reload()
     cy.wait(1000)
+    cy.screenshot(
+      `images/cypress/${feature}_${ghost_version}/create_page_atras`
+    );   
     cy.get("div.koenig-editor__editor p").should("have.text", description);
 });
 
   it("Como usuario administrador me logeo e intento editar una pagina y la publico", () => {
     const title = faker.name.jobTitle();
     const description = faker.lorem.paragraph();
-    cy.goAdminAndLogin();
+    cy.intercept("/ghost/api/admin/settings").as("saveSettings");
+    cy.goAdminAndLogin(feature);
+    cy.wait(3000);
     cy.visit(
       "http://uniandes.ingenio.com.co:2368/ghost/#/pages/"
     );
+    cy.wait(3000);
+    cy.screenshot(
+      `images/cypress/${feature}_${ghost_version}/published_pages_list`
+    );
     cy.get("h3.gh-content-entry-title").first().click();
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/edit_pages`);
+
     cy.get("textarea.gh-editor-title").clear().type(title);
     cy.get("article.koenig-editor").clear().type(description);
-    cy.get("button.gh-publish-trigger").click();
+    cy.get("button.gh-editor-save-trigger").click();
+    cy.get("span.gh-notification-actions a")
+      .invoke("removeAttr", "target")
+      .click();
+    cy.wait(3000);
+    cy.screenshot(`images/cypress/${feature}_${ghost_version}/edited_pages`);
+
     cy.get("button.gh-btn.gh-btn-black.gh-btn-large").click();
     cy.get("button.gh-btn.gh-btn-pulse").click();
   });
